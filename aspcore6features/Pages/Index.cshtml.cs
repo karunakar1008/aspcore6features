73b27653b2224df441b45c409225c3106b430288
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-08 16:40:52 +0530

    features: How to access appsettings and nested appsettings How to read values appsettings and diplay in page

--- aspcore6features/Pages/Index.cshtml.cs
+++ aspcore6features/Pages/Index.cshtml.cs
@@ -6,15 +6,23 @@ namespace aspcore6features.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,IConfiguration configuration)
         {
             _logger = logger;
+            this._configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            var myKeyValue= _configuration["MyKey"];
+            var firstValue = _configuration["Nested:FirstValue"];
+            var secondValue = _configuration["Nested:SecondValue"];
+            ViewData["myKey"] = myKeyValue;
+            ViewData["firstvalue"] = firstValue;
+            //ViewBag.firstvalue= firstValue; //
+            TempData["secondValue"] = secondValue;
         }
     }
 }
