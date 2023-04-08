3a073ac7b8e7cae41efa8c306b23512a9ac5971f
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-09 04:34:25 +0530

    Areas in razor pages

---
+++ aspcore6features/Areas/Company/Pages/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspcore6features.Areas.Company.Pages
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
