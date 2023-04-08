f22adfcfc3ebdb89f6cca421359183daa68c8c9b
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-08 19:52:00 +0530

    Messages in logging

--- aspcore6features/Pages/feature3/LoggingDemo.cshtml.cs
+++ aspcore6features/Pages/feature3/LoggingDemo.cshtml.cs
@@ -12,12 +12,22 @@ namespace aspcore6features.Pages.feature3
         }
         public void OnGet()
         {
-            _logger.LogTrace("1.Trace: Home page is loaded");
-            _logger.LogDebug("2. Debug: Home page is loaded");
-            _logger.LogInformation("3. Information: Home page is loaded");
-            _logger.LogWarning("4. Warning: Home page is loaded");
-            _logger.LogError("5. Error: Home page is loaded");
-            _logger.LogCritical("6. Critical: Home page is loaded");
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int c = a / b;
+            }
+            catch (Exception exception)
+            {
+                _logger.LogTrace("1.Trace: Home page loaded");
+                _logger.LogDebug("2. Debug: Home page loaded");
+                _logger.LogInformation("3. Information: Home page loaded");
+                _logger.LogWarning("4. Warning: Home page loaded");
+                _logger.LogError("5. Error: Home page loaded");
+                _logger.LogCritical("6. Critical: Home page loaded");
+            }
+           
         }
     }
 }
